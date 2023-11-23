7d291f6f0e01ce573d550e12061829edd0dbb859
glowing-o-n-e <151480653+glowing-o-n-e@users.noreply.github.com>
2023-11-23 15:03:44 +0000

    Update Tile.java

--- Tile.java
+++ Tile.java
@@ -1,2 +1,25 @@
+
+/**
+ * Class that encompasses all Tiles and dictates an actors movement on it
+ * @authors Jack Jones, Dan Cross
+ * @version 1.0
+ */
+
 public abstract class Tile {
-}
+
+    private Boolean moveability;
+
+    /**
+     * Constructor for the Tile class
+     * @param moveability Boolean input that dictates whether
+     *                    an actor can move on the tile or not
+     */
+    Tile (Boolean moveability) {
+        this.moveability = moveability;
+    }
+
+
+    public Boolean getMoveability() {
+        return this.moveability;
+    }
+
