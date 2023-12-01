a4275ef29eb91c0ab1a53569f8576d1357e92646
Uzair9 <151544913+Uzair9@users.noreply.github.com>
2023-12-01 17:49:56 +0000

    Update Actor.java

--- Actor.java
+++ Actor.java
@@ -2,13 +2,16 @@
  * Class representing actors in the game, such as players, monsters (pink balls, bugs, frogs), and blocks.
  * Actors have various behaviors and interactions in the game world.
  *
- * @author Benji Brew
- * @version 1.0
+ * @author Benji Brew, Uzair
+ * @version 1.1 
+ * Changed the class from abstract to concrete. Added new method addMoveableObeject which is supposed to help with tracking the moveable objects 
+ *(I'm not sure whether it will work like I intended so if anyone knows how to better implement a method for such behaiviour would be appreciated)
  */
-public abstract class Actor {
+public class Actor {
 
     private Position position;
     private int gameTickMovementInterval;
+    private List<Actor> moveableObjects; 
 
     /**
      * Constructor for creating a new Actor.
@@ -19,6 +22,7 @@ public abstract class Actor {
     public Actor(Position position, int gameTickMovementInterval) {
         this.position = position;
         this.gameTickMovementInterval = gameTickMovementInterval;
+        this.moveableObjects = new ArrayList<>();
     }
 
     /**
@@ -50,6 +54,15 @@ public abstract class Actor {
             setPosition(newPosition);
         }
     }
+   
+    /**
+     * Adds a movable object to the actor's list of moveable objects
+     *
+     * @param moveableObject The moveable object (Player, Monster, Block)
+     */
+    public void addMoveableObjects(Actor moveableObject) {
+        moveableObjects.add(moveableObject);
+    
 
     /**
      * Performs an action specific to the type of actor.
@@ -57,7 +70,12 @@ public abstract class Actor {
      *
      * @param currentGameTick The current game tick.
      */
-    public abstract void performAction(int currentGameTick);
+        public void performAction(int currentGameTick) {
+        for (Actor moveableObject : moveableObjects) {
+            moveableObject.performAction(currentGameTick);
+        }
+    }
+        
 
     /**
      * Determines if the actor should move based on the current game tick.
