291bb87873c832593ae5e7f2943064e8a9838473
glowing-o-n-e <151480653+glowing-o-n-e@users.noreply.github.com>
2023-12-01 16:57:22 +0000

    Update Path.java

--- Path.java
+++ Path.java
@@ -3,10 +3,20 @@ public class Path extends Tile {
   private static final boolean CAN_MOVE_ON = true;
   private static final String IMAGE_FILE = "Path.png";
 
+      /**
+     * Gets the boolean value of whether or not the path can be moved on.
+     *
+     * @return The boolean value of whether or not the path can be moved on.
+     */
   public boolean getCanMoveOn() {
     return CAN_MOVE_ON;
   }
 
+    /**
+     * Gets the image file of the path.
+     *
+     * @return The String of the image file of the path.
+     */
   public String getImageFile() {
     return IMAGE_FILE;
   }
