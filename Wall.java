84bf7ea5fe18244de9f720b7e6b18b92d858087b
glowing-o-n-e <151480653+glowing-o-n-e@users.noreply.github.com>
2023-12-01 16:59:11 +0000

    Update Wall.java

--- Wall.java
+++ Wall.java
@@ -2,10 +2,20 @@ public class Wall extends Tile {
   private static final boolean CAN_MOVE_ON = false;
   private static final String IMAGE_FILE = "Wall.png";
 
+    /**
+     * Gets the boolean value of whether or not the wall can be moved on.
+     *
+     * @return The boolean value of whether or not the wall can be moved on.
+     */
   public boolean getCanMoveOn() {
     return CAN_MOVE_ON;
   }
 
+  /**
+    * Gets the image file of the wall.
+    *
+    * @return The String of the image file of the wall.
+    */
   public String getImageFile() {
     return IMAGE_FILE;
   }
