0a6e94a31876d173694965fd13d046fc678936e4
glowing-o-n-e <151480653+glowing-o-n-e@users.noreply.github.com>
2023-12-01 17:13:25 +0000

    Update Exit.java

--- Exit.java
+++ Exit.java
@@ -2,10 +2,20 @@ public class Exit extends Tile {
   private static final boolean CAN_MOVE_ON = true;
   private static final String IMAGE_FILE = "Exit.png";
 
+      /**
+     * Gets the boolean value of whether or not the exit can be moved on.
+     *
+     * @return The boolean value of whether or not the exit can be moved on.
+     */
   public boolean getCanMoveOn() {
     return CAN_MOVE_ON;
   }
 
+    /**
+    * Gets the image file of the exit.
+    *
+    * @return The String of the image file of the exit.
+    */
   public String getImageFile() {
     return IMAGE_FILE;
   }
