11b7c9f342275dbf30ece1416708612ec1bdef9f
glowing-o-n-e <151480653+glowing-o-n-e@users.noreply.github.com>
2023-12-02 20:55:21 +0000

    Update Trap.java

--- Trap.java
+++ Trap.java
@@ -16,31 +16,66 @@ public class Trap extends Tile {
   public Trap() {
   }
 
+      /**
+     * Sets the trap as released.
+     *
+     */
   public void releaseTrap() {
     isReleased = true;
   }
 
+        /**
+     * Gets the boolean value of whether or not the trap is released.
+     *
+     * @return The boolean value of whether or not the trap is released.
+     */
   public boolean getIsReleased() {
     return isReleased;
   }
 
+        /**
+     * Gets the boolean value of whether or not the trap is connected.
+     *
+     * @return The boolean value of whether or not the trap is connected.
+     */
   public boolean getIsConnected() {
     return isConnected;
   }
 
+        /**
+     * Sets the connected button.
+     *
+     * @param the new button to be set.
+     */
   public void setConnectedButton(Button newButton) {
     connectedButton = newButton;
     isConnected = true;
   }
 
+
+        /**
+     * Gets the Button value of the connected button.
+     *
+     * @return The the Button value of the connected button.
+     */
   public Button getConnectedButton() {
     return connectedButton;
   }
 
+        /**
+     * Gets the boolean value of whether or not the trap can be moved on.
+     *
+     * @return The boolean value of whether or not the trap can be moved on.
+     */
   public boolean getCanMoveOn() {
     return CAN_MOVE_ON;
   }
 
+      /**
+    * Gets the image file of the trap.
+    *
+    * @return The String of the image file of the trap.
+    */
   public String getImageFile() {
     return IMAGE_FILE;
   }
