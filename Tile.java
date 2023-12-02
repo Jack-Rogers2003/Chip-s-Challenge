d74500c0a53488b88c4a90a3ce74b0f57212609a
Dan Cross <151852655+DanCrossie@users.noreply.github.com>
2023-12-02 18:08:42 +0000

    Update Tile.java
    created the movability attribute

--- Tile.java
+++ Tile.java
@@ -2,12 +2,15 @@
 /**
  * Class that encompasses all Tiles and dictates an actors movement on it
  * @author Jack Jones, Dan Cross
- * @version 1.0
+ * @version 1.1
  */
 
 public abstract class Tile {
-    public abstract boolean getCanMoveOn();
+    public abstract Actor[] moveability;        // an array of the actor types that can move over the given tile
     public abstract String getImageFile();
 
+    public Actor[] getMoveability() {
+        return moveability;
+    }
 }
 
