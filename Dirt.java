e6ee91b9e72ea6d316ca11b15cd4657f70a5725f
glowing-o-n-e <151480653+glowing-o-n-e@users.noreply.github.com>
2023-12-02 20:57:59 +0000

    Update Dirt.java

--- Dirt.java
+++ Dirt.java
@@ -1,3 +1,7 @@
+/**
+ * This class creates the Dirt tile
+ */
+
 public class Dirt extends Tile {
   private static final boolean CAN_MOVE_ON = true;
   private static final String IMAGE_FILE = "Dirt.png";
