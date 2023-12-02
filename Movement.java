ec5de064e26c4a07e06a6ecfdaaaaf6f3232e7cd
Dan Cross <151852655+DanCrossie@users.noreply.github.com>
2023-12-02 18:05:08 +0000

    Update Movement.java

--- Movement.java
+++ Movement.java
@@ -1,2 +1,36 @@
+/* A class to handle the movement of all actor classes
+ * All actor positions passed through the actor class rather than position class
+ * @author Dan Cross
+ * @version 1.0
+ * 
+ * side note - javadoc not finished and my implementation of this class requires some tinkering
+ * with other classes (some of which i've done whereas some still needs doing)
+ */
 public class Movement {
+
+    private Actor movingActor;
+
+    public Movement(Actor movingActor){
+        this.movingActor = movingActor;
+    }
+
+
+    public void moveToPosition(Integer[] desiredPosition) {
+        if (canMove( desiredPosition) == false) {
+            //cannot move
+        } else {
+            movingActor.setPosition(desiredPosition);
+        }
+    }
+
+    public Boolean canMove(Integer[] desiredPosition) {
+        //if actor can move on tile
+        Tile tile = Position.getTileAtPosition(desiredPosition);
+        for (int i = 0; i < tile.getMoveability().length; i++) {
+            if (tile.getMoveability()[i] == this.movingActor ) {
+                return true;
+            } else return false;
+        }
+    }
+
 }
