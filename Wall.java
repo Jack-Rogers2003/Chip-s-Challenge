1c1a2ee5212f9ed375c07cab736f37136eae1ab0
Jack-Rogers2003 <150918392+Jack-Rogers2003@users.noreply.github.com>
2023-12-03 13:39:03 +0000

    Update Wall.java

--- Wall.java
+++ Wall.java
@@ -1,6 +1,6 @@
 public class Wall extends Tile {
   private static final boolean CAN_MOVE_ON = false;
-  private static final String IMAGE_FILE = "Wall.png";
+  private static final String IMAGE_FILE = "wall.png";
 
   public boolean getCanMoveOn() {
     return CAN_MOVE_ON;
