50cb1692f1a4fe505d63a5cbdd60e35c8d7e5c52
Dan Cross <151852655+DanCrossie@users.noreply.github.com>
2023-12-03 16:11:14 +0000

    Update Movement.java
    Implemented Jack Jones' method isOutOfBounds into the canMove method

--- Movement.java
+++ Movement.java
@@ -1,10 +1,10 @@
-/* A class to handle the movement of all actor classes
+/**
+ * A class to handle the movement of all actor classes
  * All actor positions passed through the actor class rather than position class
- * @author Dan Cross
- * @version 1.0
+ *
+ * @author Dan Cross, Jack Jones
+ * @version 1.1
  * 
- * side note - javadoc not finished and my implementation of this class requires some tinkering
- * with other classes (some of which i've done whereas some still needs doing)
  */
 public class Movement {
 
@@ -14,17 +14,36 @@ public class Movement {
         this.movingActor = movingActor;
     }
 
-
+    /**
+     * Method to change a given actors position if possible
+     *
+     * @param desiredPosition Integer[] is the position the moving actor wants to move to
+     */
     public void moveToPosition(Integer[] desiredPosition) {
         if (canMove( desiredPosition) == false) {
-            //cannot move
-        } else {
-            movingActor.setPosition(desiredPosition);
+            desiredPosition = movingActor.getPosition()
         }
+        movingActor.setPosition(desiredPosition);
     }
 
-    public Boolean canMove(Integer[] desiredPosition) {
-        //if actor can move on tile
+    /**
+     * Method to check if a given positon will accept the actor
+     *
+     * @param desiredPosition Integer[] x,y value of intended next position
+     * @return boolean value that if true states a player can move to given position
+     */
+    private Boolean canMove(Integer[] desiredPosition) {
+       if (acceptTile(desiredPosition) == true && isOutOfBounds(desiredPosition, board) == false) {
+           return true;
+       }
+    }
+    /**
+     * Method to check if given position has a tile appropriate for movement
+     *
+     * @param desiredPosition Integer[] x,y value of intended next position
+     * @return boolean value that if true states a tile is acceptable
+     */
+    private Boolean acceptTile(Integer[] desiredPosition) {
         Tile tile = Position.getTileAtPosition(desiredPosition);
         for (int i = 0; i < tile.getMoveability().length; i++) {
             if (tile.getMoveability()[i] == this.movingActor ) {
@@ -32,5 +51,24 @@ public class Movement {
             } else return false;
         }
     }
+
+    /** 
+     * Checks if given position is out of bounds of the board in play
+     *
+     * @param desiredPosition Integer[] x,y value of intended next position 
+     * @param board Board value of current board being played on
+     * @return boolean value of whether the position is OOB
+     */
+    private boolean isOutOfBounds(Integer[] desiredPosition, Board board) {
+        Integer[] boardSize = board.getBoardSize();
+        boolean xOOB = (desiredPosition[0] > boardSize[0]) || (desiredPosition[0] < (boardSize[0] - boardSize[0]));
+        boolean yOOB = (desiredPosition[1] > boardSize[1]) || (desiredPosition[1] < (boardSize[1] - boardSize[1]));
+        if (xOOB || yOOB) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 
 }
