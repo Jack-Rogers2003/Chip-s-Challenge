8775662f8933d8cd42fa490da39c8499419b85bd
BenjiBr3w <140737605+BenjiBr3w@users.noreply.github.com>
2023-12-07 11:19:16 +0000

    Update Dirt.java
    Added javadoc

--- Dirt.java
+++ Dirt.java
@@ -1,16 +1,20 @@
 /**
- * This class creates the Dirt tile
- */
+  * This class creates the Dirt tile
+  * Specifies the conditions the dirt tile creates for an actor
+  * @author Jack Jones, Benji Brew
+  * @version 1.1
+  */
 
 public class Dirt extends Tile {
+  
   private static final boolean CAN_MOVE_ON = true;
   private static final String IMAGE_FILE = "Dirt.png";
 
   /**
-     * Gets the boolean value of whether or not the dirt can be moved on.
-     *
-     * @return The boolean value of whether or not the dirt can be moved on.
-     */
+    * Gets the boolean value of whether or not the dirt can be moved on.
+    *
+    * @return The boolean value of whether or not the dirt can be moved on.
+    */
   public boolean getCanMoveOn() {
     return CAN_MOVE_ON;
   }
