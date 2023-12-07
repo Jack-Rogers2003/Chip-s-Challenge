d6e3463a1d8510e0f19f58bd173bfabbd4f42cb2
BenjiBr3w <140737605+BenjiBr3w@users.noreply.github.com>
2023-12-07 11:36:44 +0000

    Update Frog.java
    Javadoc

--- Frog.java
+++ Frog.java
@@ -1,2 +1,9 @@
+/**
+  * This class represents the frog monster
+  * Specifies the actions a frog can perform
+  * @author Benji Brew
+  * @version 1.1
+ /*
+
 public class Frog extends Monster {
 }
