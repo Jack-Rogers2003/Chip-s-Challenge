695af2c5695dac42d606c433111d01fa7b7a6dc9
BenjiBr3w <140737605+BenjiBr3w@users.noreply.github.com>
2023-12-08 14:01:05 +0000

    Javadoc

--- Item.java
+++ Item.java
@@ -1,3 +1,10 @@
+/**
+ * This is the item class that is th superclass off all other items
+ * Specifies abstract variables that other items will inherit from
+ * @author Benji Brew, Jack Rogers
+ * @version 1.1
+ */
+
 public abstract class Item {
   public abstract Item getItem();
   public abstract String getImageFile();
