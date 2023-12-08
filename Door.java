985cc70c87c1fb6686a114cadb20c0d60cbbeee5
BenjiBr3w <140737605+BenjiBr3w@users.noreply.github.com>
2023-12-08 15:33:22 +0000

    Javadoc

--- Door.java
+++ Door.java
@@ -1,8 +1,19 @@
+/**
+ * This class represents the door tile and the conditions that it creates
+ * @author Benji Brew, Jack Rogers
+ * @version 1.1
+ */
+
 public class Door extends Tile {
   private static final boolean CAN_MOVE_ON = false;
   private final String colour;
   private String imageFile;
 
+  /**
+   * Contructer for the door tile that specifies which colour door to use
+   *
+   * @param newColour the colour of the door
+   */
   public Door(String newColour) {
     colour = newColour;
     switch (colour) {
@@ -13,14 +24,29 @@ public class Door extends Tile {
     }
   }
 
+  /**
+   * Method that retrieves the image file of the door
+   *
+   * @return the image file
+   */
   public String getImageFile() {
     return imageFile;
   }
 
+  /**
+   * Method that checks if an actor can move onto a door
+   *
+   * @return the boolean value of CAN_MOVE_ON
+   */
   public boolean getCanMoveOn() {
     return CAN_MOVE_ON;
   }
 
+  /**
+   * Method that gets the colour of the door
+   *
+   * @return the colour of the door
+   */
   public String getColour() {
     return colour;
   }
