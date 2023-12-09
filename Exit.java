7d0d0195918f9c0097392d9db3670a49121d4a6a
BenjiBr3w <140737605+BenjiBr3w@users.noreply.github.com>
2023-12-09 12:48:31 +0000

    Update Exit.java

--- Exit.java
+++ Exit.java
@@ -1,3 +1,10 @@
+/**
+ * This class represents the Exit tile and has methods to check how an actor may
+ * or may not interact with it
+ * @author Benji Brew, Jack Rogers
+ * @version 1.1
+ */
+
 public class Exit extends Tile {
   private static final boolean CAN_MOVE_ON = true;
   private static final String IMAGE_FILE = "Exit.png";
