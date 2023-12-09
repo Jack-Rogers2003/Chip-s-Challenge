b8f0b4f7cda08225ce0b6aeb780cd5de48b43679
BenjiBr3w <140737605+BenjiBr3w@users.noreply.github.com>
2023-12-09 13:00:06 +0000

    Javadoc

--- Exit.java
+++ Exit.java
@@ -9,10 +9,20 @@ public class Exit extends Tile {
   private static final boolean CAN_MOVE_ON = true;
   private static final String IMAGE_FILE = "Exit.png";
 
+  /**
+   * Method that checks if an actor can move onto an exit tile
+   *
+   * @return the boolean value of CAN_MOVE_ON
+   */
   public boolean getCanMoveOn() {
     return CAN_MOVE_ON;
   }
 
+  /**
+   * Method that retrieves the image on an exit tile
+   *
+   * @return the image file
+   */
   public String getImageFile() {
     return IMAGE_FILE;
   }
