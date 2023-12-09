a5e66549b3b528a33b699d36ec473ec2b51f7eaf
Annie <125764734+tinkeree@users.noreply.github.com>
2023-12-09 14:52:20 +0000

    Update Chip.java

--- Chip.java
+++ Chip.java
@@ -1,6 +1,16 @@
 public class Chip extends Item {
 
+    /**
+    * Class that creates a chip item
+    * @author Jack Rogers
+    * @version 1.1
+    **/
+
     private static final String IMAGE_FILE = "chip.png";
+
+    /** 
+    Method that returns the value of the image
+    **/
 
     public String getImageFile() {
         return IMAGE_FILE;
