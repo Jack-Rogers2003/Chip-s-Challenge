2cb0078e3a52050c12837f902a01ddbc2952c0f7
glowing-o-n-e <151480653+glowing-o-n-e@users.noreply.github.com>
2023-12-09 14:21:16 +0000

    Add files via upload

--- Save.java
+++ Save.java
@@ -1,8 +1,11 @@
+package com.example.goingtoblowmybrainsout;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-//NOT FINISHED. JUST SHOWING PROGRESS
+
+//NOT FINISHED. JUST SHOWING PROGRESS. LOTS OF JUNK CLASSES. WHATEVER
 public class Save {
     private static final String SAVE_FILE = "Save.txt";
     private int tickCounter = -1;
@@ -13,16 +16,16 @@ public class Save {
     private static final String CURRENT_DIRECTORY = System.getProperty("user.dir");
     private static final String LEVEL_FILE_PATH = CURRENT_DIRECTORY +
             "\\Levels\\";
+    private String levelFile = "";
     public void saveGame(Player player, String levelFile) throws IOException {
         playerItems = player.getPlayerItems();
         tickCounter = 5; //BoardGUI.getTickCount();
-        //width = board
+        this.levelFile = levelFile;
         Scanner fileReader = new Scanner(new File(LEVEL_FILE_PATH
                 + levelFile + ".txt"));
-
         saveFile = new File("Save.txt");
         FileWriter saveWriter = new FileWriter(saveFile, false);
-        saveWriter.write( writeLevel(levelFile) + "\n" + tickCounter + "\n" + itemALToString((ArrayList<Item>) playerItems));
+        saveWriter.write( writeLevel(levelFile) + tickCounter + "\n" + itemALToString((ArrayList<Item>) playerItems));
         saveWriter.close();
     }
 
@@ -40,17 +43,33 @@ public class Save {
         return contents;
     }
 
-
     public String writeLevel (String levelFile) throws FileNotFoundException {
         String contents = "";
         Scanner fileReader = new Scanner(new File(LEVEL_FILE_PATH
                 + levelFile + ".txt"));
         while (fileReader.hasNextLine()) {
             String data = fileReader.nextLine();
-            contents = contents + data;
+            contents = contents + data + "\n";
         }
         return contents;
     }
+
+    public String createRow (String fileRow, int nextRow) {
+        String contents = "";
+        String[] rowElements = fileRow.split("_");
+        for(int i = 0; i <= 5; i++) { //BoardGUI.getBoardSize()[0] - 1
+            int[] currentSquarePosition = new int[]{i,nextRow};
+            String[] propertiesOfSquare = rowElements[i].split(";");
+            contents = contents + propertiesOfSquare;
+            if(rowElements[i].length() > 1) {
+                for(int j = 1; j < propertiesOfSquare.length; j++) {
+                    contents = contents + propertiesOfSquare;
+                }
+            }
+        }
+        return contents;
+    }
+
     public void loadGame (Player player) throws IOException {
          Scanner saveReader = new Scanner(saveFile);
          while (saveReader.hasNextLine()) {
@@ -59,5 +78,9 @@ public class Save {
          }
         saveReader.close();
 
+    }
+
+    public String getLevelFile() {
+        return levelFile;
     }
 }
