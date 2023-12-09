3629679a36da738230c1821f4d0571b60c59cbed
glowing-o-n-e <151480653+glowing-o-n-e@users.noreply.github.com>
2023-12-09 15:20:05 +0000

    Update Load.java

--- Load.java
+++ Load.java
@@ -1,7 +1,10 @@
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Load {
     private static final String SAVE_FILE = "Save.txt";
@@ -9,7 +12,6 @@ public class Load {
     private static List<Item> playerItems = null;
     private String stringContents = "";
     private static File saveFile = null;
-    private int width = 0;
     private static final String CURRENT_DIRECTORY = System.getProperty("user.dir");
     private static final String LEVEL_FILE_PATH = CURRENT_DIRECTORY +
             "\\Levels\\";
@@ -17,14 +19,39 @@ public class Load {
     public static void loadGame(Player player, Save save) throws IOException {
         playerItems = player.getPlayerItems();
         tickCounter = 5; //BoardGUI.getTickCount();
-        Scanner fileReader = new Scanner(new File(LEVEL_FILE_PATH
-                + save.getLevelFile() + ".txt"));
-        saveFile = new File("Save.txt");
+        saveFile = save.getSaveFile();
+        Scanner fileReader = new Scanner(saveFile);
         int size[] = new int[2];
         size[0] = Integer.parseInt(fileReader.nextLine());
         size[1] = Integer.parseInt(fileReader.nextLine());
         int timer = Integer.parseInt(fileReader.nextLine());
+        String level = ""; //new positions
+        int height = size[1];
+        for (int i = 0; i < height; i++) {
+            level = level + fileReader.nextLine() + "\n";
+        }
+        int tickCount = Integer.parseInt(fileReader.nextLine());
+        inventoryPutter(fileReader.nextLine(), save);
         BoardGUI.setBoardSize(size);
         BoardGUI.setGameTime(timer);
+        //BoardGUI.setTicks(tickCount);
+    }
+
+    public static void  inventoryPutter(String inventoryLine, Save save) {
+        String[] output = inventoryLine.split("-", 10);
+        playerItems.clear();
+        for (int i = 1; i < output.length; i++) {
+            if (output[i].equals("Kred")) {
+                playerItems.add(new Key("R"));
+            } else if (output[i].equals("Kblue")) {
+                playerItems.add(new Key("B"));
+            } else if (output[i].equals("Kyellow")) {
+                playerItems.add(new Key("Y"));
+            } else if (output[i].equals("Kgreen")) {
+                playerItems.add(new Key("G"));
+            } else if (output[i].equals("C")) {
+                playerItems.add(new Chip());
+            }
+        }
     }
 }
