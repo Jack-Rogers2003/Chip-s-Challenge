6dd0dae19be3dfc73ffa4a86ebf8bf19faf5ca29
Dan Cross <151852655+DanCrossie@users.noreply.github.com>
2023-12-10 19:31:07 +0000

    Update javadoc for Block.java

--- Block.java
+++ Block.java
@@ -1,6 +1,16 @@
+/**
+ * A class representing a block-type actor. This class is a subclass of Actor
+ *
+ * @version 1.0
+ */
 public class Block {
     private static final String IMAGE_FILE = "block.png";
-
+    
+    /**
+    * A method to retrieve the private attribute IMAGE_FIlE
+    *
+    * @returns IMAGE_FILE
+    */
     public String getImageFile() {
         return IMAGE_FILE;
     }
